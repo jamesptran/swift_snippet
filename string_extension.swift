e121ffdfa3580bfb68921269883f47cdcf4490e9
Phuc Tran <jamesptran96@gmail.com>
2022-05-29 00:26:44 -0700

    Update string_extension.swift

--- string_extension.swift
+++ string_extension.swift
@@ -1,8 +1,43 @@
-extension String {
+extension StringProtocol {
+    // str[i]
+    subscript(offset: Int) -> Character { 
+        return self[index(startIndex, offsetBy: offset)] 
+    }
+
+    // str[i..<j]
+    subscript(range: Range<Int>) -> SubSequence {
+        let start = index(self.startIndex, offsetBy: range.lowerBound)
+        return self[start..<index(start, offsetBy: range.count)]
+    }
+
+    // str[i...j]
+    subscript(range: ClosedRange<Int>) -> SubSequence {
+        let start = index(self.startIndex, offsetBy: range.lowerBound)
+        return self[start..<index(start, offsetBy: range.count)]
+    }
+
+    // str[i...]
+    subscript(range: PartialRangeFrom<Int>) -> SubSequence { 
+        return self[index(startIndex, offsetBy: range.lowerBound)...] 
+    }
+
+    // str[...j]
+    subscript(range: PartialRangeThrough<Int>) -> SubSequence { 
+        return self[...index(startIndex, offsetBy: range.upperBound)] 
+    }
+
+    // str[..<j]
+    subscript(range: PartialRangeUpTo<Int>) -> SubSequence { 
+        return self[..<index(startIndex, offsetBy: range.upperBound)] 
+    }
+    
     func replaceNew(_ index: Int, _ char: String) -> String {
         return String(self.prefix(index)) + char + String(self.suffix(self.count - index - 1))
     }
+}
 
+
+extension String {
     mutating func replace(_ index: Int, _ char: String) {
         let ind = self.index(self.startIndex, offsetBy: index)
         self.replaceSubrange(ind...ind, with: char)
